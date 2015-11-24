a0b55cdbd8c3d4c25acdb052543f8ed89d006e1f
Ed Goodwin <ed.goodwin@gmail.com>
2015-11-24 00:07:37 -0600

    added summarise_each()  to get valuation methods by sector

--- r2kMetrics.r
+++ r2kMetrics.r
@@ -6,12 +6,37 @@ library(dplyr)
 library(data.table)
 
 setwd("/Users/egoodwin/Documents/active/code/R/r2kMetrics/")
-r2k.df = read.csv("r2k_screener.csv")
+
+## read in data file
+r2k.df = read.csv("r2k_screener.csv", header=TRUE, stringsAsFactors=FALSE)
+
+## convert column names
 names(r2k.df) = c("Symbol", "Name", "Sector", "Industry", "Market.Cap", 
                   "Ent.Val", "PEratio", "EV.EBITDA", "PSratio", "PBratio")
 
-dt.r2k = data.table(r2k.df)
-setkey(dt.r2k, Industry, PEratio)
-ddply(dt.r2k, "Industry", summarise, pe = mean(PEratio), evebitda = mean(EV.EBITDA))
+## data conversion
+r2k.df$Market.Cap = as.numeric(r2k.df$Market.Cap)
+r2k.df$Ent.Val = as.numeric(r2k.df$Ent.Val)
+r2k.df$PEratio = as.numeric(r2k.df$PEratio)
+r2k.df$EV.EBITDA = as.numeric(r2k.df$EV.EBITDA)
+r2k.df$PSratio = as.numeric(r2k.df$PSratio)
+r2k.df$PBratio = as.numeric(r2k.df$PBratio)
 
-summarise(dt.r2k, mean(PEratio))
+## set NA to 0
+r2k.df[is.na(r2k.df)] = 0
+
+## get total mkt cap and ent val for each industry
+r2kTotMktCap = r2k.df %>% group_by(Sector) %>% summarise(mktcaptot = sum(Market.Cap, na.rm=TRUE))
+r2kTotEntVal = r2k.df %>% group_by(Sector) %>% summarise(mktcaptot = sum(Market.Cap, na.rm=TRUE))
+
+## add total market cap and ent values as columns by Sector
+q = c(0.34, 0.50, 0.66)
+
+r2ksumm = r2k.df %>% 
+  group_by (Sector) %>% 
+  summarise_each(funs(median), 
+                 EV.EBITDA, 
+                 PEratio,
+                 PSratio,
+                 PBratio)
+
